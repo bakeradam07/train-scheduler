846977677ded7e301025d5118f79812b1f561210
Adam Baker <albcodingbootcamp@gmail.com>
2019-10-23 15:57:54 -0400

    added firebase config in js file and updated some id's on html

---
+++ javascript/javascript.js
@@ -0,0 +1,13 @@
+var config = {
+    apiKey: "AIzaSyBio12_wRkOWn0XKnYkAwhemj22LVLAcZY",
+    authDomain: "oct-12-test.firebaseapp.com",
+    databaseURL: "https://oct-12-test.firebaseio.com",
+    projectId: "oct-12-test",
+    storageBucket: "oct-12-test.appspot.com",
+    messagingSenderId: "1058452132622",
+    appId: "1:1058452132622:web:6914b05dd6ff8277c2bd5f"
+  };
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
